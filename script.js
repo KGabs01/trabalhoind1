bc3178952996016cecbb7939b3df055d57efd33c
KGabs01 <146892998+KGabs01@users.noreply.github.com>
2023-10-10 21:07:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector("h1").textContent="Programação e Sistemas de Informação";
+
+document.querySelector("footer").innerHTML+=`<ahref="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>`;
